1ac661cb3a56096693d4a2f010afe4f4b1e088e0
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2023-03-17 06:45:59 -0300

    Ajustes

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -5,8 +5,8 @@ import SwiperCore, { Navigation, Pagination } from 'swiper';
 // install Swiper modules
 SwiperCore.use([Navigation, Pagination]);
 
-export function Banner() {
-  return (
+
+/*
     <Swiper spaceBetween={0} slidesPerView={1} navigation pagination={{ clickable: true }} loop={true} speed={1500} >
       <SwiperSlide>
         <img src="/banner-1.webp" alt="banner-1" />
@@ -18,5 +18,11 @@ export function Banner() {
         <img src="/banner-3.png" alt="banner-3" />
       </SwiperSlide>
     </Swiper>
+*/
+
+export function Banner() {
+  return (
+    <>
+    </>
   )
 }
