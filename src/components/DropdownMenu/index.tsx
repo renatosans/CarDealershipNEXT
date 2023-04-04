5ff7d8aaa13a75a54a8733b3c86c1a0ef1223b45
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2023-04-04 18:28:57 -0300

    Ajustes

--- src/components/DropdownMenu/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles.module.scss';
-
-interface DropdownMenuProps {
-  children: React.ReactNode;
-}
-
-export function DropdownMenu({ children }: DropdownMenuProps) {
-  return <div className={styles.dropdown}>{children}</div>;
-}
--- src/components/MenuSeparatorVertical/index.tsx
+++
@@ -1,5 +0,0 @@
-import styles from './styles.module.scss';
-
-export function MenuSeparatorVertical() {
-  return <div className={styles.separator}></div>;
-}
