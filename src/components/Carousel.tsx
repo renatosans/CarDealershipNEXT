ae26253512b1a7073c088dcf33f99d2e30d1c186
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2023-04-05 19:11:40 -0300

    Ajustes

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -11,13 +11,13 @@ export default function Carousel() {
   return (
       <Swiper spaceBetween={0} slidesPerView={1} navigation={true} pagination={true} modules={[Navigation, Pagination]} autoplay={{ delay: 1500, disableOnInteraction: false }} >
         <SwiperSlide className='carousel-slide'>
-          <img style={{width: "100%"}} src="/banner-1.png" alt="banner-1" />
+          <img style={{width: "100%"}} src="/img/banner-1.png" alt="banner-1" />
         </SwiperSlide>
         <SwiperSlide className='carousel-slide'>
-          <img style={{width: "100%"}} src="/banner-2.png" alt="banner-2" />
+          <img style={{width: "100%"}} src="/img/banner-2.png" alt="banner-2" />
         </SwiperSlide>
         <SwiperSlide className='carousel-slide'>
-          <img style={{width: "100%"}} src="/banner-3.png" alt="banner-3" />
+          <img style={{width: "100%"}} src="/img/banner-3.png" alt="banner-3" />
         </SwiperSlide>
       </Swiper>
   )
